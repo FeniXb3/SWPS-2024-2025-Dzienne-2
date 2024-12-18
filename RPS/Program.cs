fe1a8d076476bbe1a6977c79f24a19f930ff20df
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-18 15:37:11 +0100

    Add GetSign method RPS

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-string firstAllowedSign = "fire";
+﻿string firstAllowedSign = "fire";
 string secondAllowedSign = "water";
 string thirdAllowedSign = "grass";
 
@@ -9,43 +8,29 @@ int secondPlayerPoints = 0;
 
 while (true)
 {
-    // Console.WriteLine("Graczu 1, podaj znak (rock/paper/scissors)");
-    // Console.WriteLine("Graczu 1, podaj znak (" + firstAllowedSign + "/" + secondAllowedSign + "/" + thirdAllowedSign + ")");
-    // Console.WriteLine($"Graczu 1, podaj znak ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
-    // Console.WriteLine($"Graczu 1, podaj znak ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
-    Console.WriteLine($"Graczu 1, podaj znak ({string.Join("/", allowedSigns)})");
-    // typ nazwa;
-    // typ nazwa = wartość;
-    // string firstSign = (Console.ReadLine() ?? string.Empty).ToLower();
-    string firstSign = Console.ReadLine()?.ToLower() ?? string.Empty;
-    // firstSign = firstSign.ToLower();
+    // Console.WriteLine($"Graczu 1, podaj znak ({string.Join("/", allowedSigns)})");
+    // string firstSign = Console.ReadLine()?.ToLower() ?? string.Empty;
 
-    // liczby całkowite - typ int
-
-    // while (firstSign != firstAllowedSign && firstSign != secondAllowedSign && firstSign != thirdAllowedSign)
-    // while (firstSign != allowedSigns[0] && firstSign != allowedSigns[1] && firstSign != allowedSigns[2])
-    while (!allowedSigns.Contains(firstSign))
-    {
-        Console.WriteLine("Niepoprawny znak!");
-        // Console.WriteLine($"Wpisz POPRAWNY znak, graczu 1 ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
-        Console.WriteLine($"Wpisz POPRAWNY znak, graczu 1 ({string.Join("/", allowedSigns)})");
-        firstSign = Console.ReadLine()?.ToLower() ?? string.Empty;
-    }
+    // while (!allowedSigns.Contains(firstSign))
+    // {
+    //     Console.WriteLine("Niepoprawny znak!");
+    //     Console.WriteLine($"Wpisz POPRAWNY znak, graczu 1 ({string.Join("/", allowedSigns)})");
+    //     firstSign = Console.ReadLine()?.ToLower() ?? string.Empty;
+    // }
 
 
-    // Console.WriteLine($"Graczu 2, podaj znak ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
-    Console.WriteLine($"Graczu 2, podaj znak ({string.Join("/", allowedSigns)})");
-    string secondSign = Console.ReadLine()?.ToLower() ?? string.Empty;
-    Console.WriteLine(secondSign);
+    // Console.WriteLine($"Graczu 2, podaj znak ({string.Join("/", allowedSigns)})");
+    // string secondSign = Console.ReadLine()?.ToLower() ?? string.Empty;
+    // Console.WriteLine(secondSign);
 
-    // while (secondSign != firstAllowedSign && secondSign != secondAllowedSign && secondSign != thirdAllowedSign)
-    while (!allowedSigns.Contains(secondSign))
-    {
-        Console.WriteLine("Niepoprawny znak!");
-        // Console.WriteLine($"Wpisz POPRAWNY znak, graczu 2 ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
-        Console.WriteLine($"Wpisz POPRAWNY znak, graczu 2 ({string.Join("/", allowedSigns)})");
-        secondSign = Console.ReadLine()?.ToLower() ?? string.Empty;
-    }
+    // while (!allowedSigns.Contains(secondSign))
+    // {
+    //     Console.WriteLine("Niepoprawny znak!");
+    //     Console.WriteLine($"Wpisz POPRAWNY znak, graczu 2 ({string.Join("/", allowedSigns)})");
+    //     secondSign = Console.ReadLine()?.ToLower() ?? string.Empty;
+    // }
+    string firstSign = GetSign(1);
+    string secondSign = GetSign(2);
 
     if (firstSign == secondSign)
     {
@@ -54,13 +39,11 @@ while (true)
     else if ((firstSign == firstAllowedSign && secondSign == thirdAllowedSign) || (firstSign == secondAllowedSign && secondSign == firstAllowedSign) || (firstSign == thirdAllowedSign && secondSign == secondAllowedSign))
     {
         Console.WriteLine("Gracz 1 MISZCZ!");
-        // firstPlayerPoints = firstPlayerPoints + 1;
         firstPlayerPoints += 1;
     }
     else
     {
         Console.WriteLine("Gracz 2 MISZCZ");
-        // secondPlayerPoints = secondPlayerPoints + 1;
         secondPlayerPoints += 1;
     }
 
@@ -75,3 +58,20 @@ while (true)
 }
 
 Console.WriteLine("Dziękuję za grę");
+
+
+// typ_wartości_zwracanej nazwa_metody(informacje_wejściowe)
+string GetSign(int playerNumber)
+{
+    Console.WriteLine($"Graczu {playerNumber}, podaj znak ({string.Join("/", allowedSigns)})");
+    string sign = Console.ReadLine()?.ToLower() ?? string.Empty;
+
+    while (!allowedSigns.Contains(sign))
+    {
+        Console.WriteLine("Niepoprawny znak!");
+        Console.WriteLine($"Wpisz POPRAWNY znak, graczu {playerNumber} ({string.Join("/", allowedSigns)})");
+        sign = Console.ReadLine()?.ToLower() ?? string.Empty;
+    }
+
+    return sign;
+}
